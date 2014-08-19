1e4813e7810420d1cc16151a1523c583fbd13e17
dmckernan416 <daniellemckernan24@hotmail.com>
2014-08-19 14:12:06 +0100

    Running robot/refuel.py

--- robot/refuel.py
+++ robot/refuel.py
@@ -5,6 +5,7 @@ from tealight.robot import (move,
                             smell, 
                             left_side, 
                             right_side)
-move
+
 
 # Add your code here
+move()
